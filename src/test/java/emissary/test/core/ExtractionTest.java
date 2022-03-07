e0c1746955687f8b162f1152f28afc00258a23a1
Sam Bishop <sambishop@hawkeye.fios-router.home>
2022-03-07 14:09:26 -0500

    Fixed * imports

--- src/test/java/emissary/test/core/ExtractionTest.java
+++ src/test/java/emissary/test/core/ExtractionTest.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.ArrayList;
