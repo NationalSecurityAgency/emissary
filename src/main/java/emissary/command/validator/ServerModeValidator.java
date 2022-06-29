d2d772eb9c5f3c8d7b217d25817e48e45d19eec2
Sam Bishop <sambishop@hawkeye.fios-router.home>
2022-06-29 10:28:28 -0400

    Updated ServerModeValidator based on requested changes.

--- src/main/java/emissary/command/validator/ServerModeValidator.java
+++ src/main/java/emissary/command/validator/ServerModeValidator.java
@@ -2,13 +2,8 @@ package emissary.command.validator;
 
 import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.ParameterException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ServerModeValidator implements IParameterValidator {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ServerModeValidator.class);
-
     @Override
     public void validate(String name, String value) throws ParameterException {
         switch (value) {
@@ -16,7 +11,6 @@ public class ServerModeValidator implements IParameterValidator {
             case "standalone":
                 break;
             default:
-                // LOG.error("Unknown mode: {}", value);
                 throw new IllegalArgumentException("Unknown mode: " + value);
         }
     }
