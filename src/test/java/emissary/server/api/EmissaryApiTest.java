f11e1c1f889f7c6f6d0d1e4726816cd00c3a9612
dev-mlb <dev-mlb@users.noreply.github.com>
2022-07-14 16:17:40 -0400

    mvc cleanup and updates to mvc/api tests

---
+++ src/test/java/emissary/server/api/EmissaryApiTest.java
@@ -0,0 +1,284 @@
+package emissary.server.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import javax.ws.rs.core.Response;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.health.HealthCheck;
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.google.common.collect.Sets;
+import emissary.client.response.AgentsResponseEntity;
+import emissary.client.response.MapResponseEntity;
+import emissary.client.response.PlacesResponseEntity;
+import emissary.command.ServerCommand;
+import emissary.core.EmissaryException;
+import emissary.core.MetricsManager;
+import emissary.core.Namespace;
+import emissary.directory.DirectoryPlace;
+import emissary.directory.EmissaryNode;
+import emissary.pool.AgentPool;
+import emissary.pool.MobileAgentFactory;
+import emissary.server.EmissaryServer;
+import emissary.server.mvc.EndpointTestBase;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmissaryApiTest extends EndpointTestBase {
+
+    @BeforeEach
+    public void setup() {
+        EmissaryServer mockServer = mock(EmissaryServer.class);
+        EmissaryNode node = spy(new EmissaryNode());
+        ServerCommand srvCmd = mock(ServerCommand.class);
+
+        doReturn("localhost").when(node).getNodeName();
+        doReturn(8001).when(node).getNodePort();
+        when(mockServer.getNode()).thenReturn(node);
+        when(mockServer.getServerCommand()).thenReturn(srvCmd);
+        when(srvCmd.getConfig()).thenReturn(Paths.get("/path/to/project/config"));
+        when(srvCmd.getProjectBase()).thenReturn(Paths.get("/path/to/project"));
+        when(srvCmd.getOutputDir()).thenReturn(Paths.get("/path/to/project/output"));
+        when(srvCmd.getBinDir()).thenReturn(Paths.get("/path/to/project/bin"));
+        when(srvCmd.getHost()).thenReturn("localhost");
+        when(srvCmd.getPort()).thenReturn(8001);
+        when(srvCmd.getScheme()).thenReturn("https");
+
+        Namespace.bind("EmissaryServer", mockServer);
+    }
+
+    @AfterEach
+    public void cleanup() {
+        Namespace.clear();
+    }
+
+    @Test
+    void agents() {
+
+        String[] expectedAgents = {"MobileAgent-00: Idle", "MobileAgent-01: Idle", "MobileAgent-02: Idle", "MobileAgent-03: Idle",
+                "MobileAgent-04: Idle", "MobileAgent-05: Idle", "MobileAgent-06: Idle", "MobileAgent-07: PHASE.FORM.Place", "MobileAgent-08: Idle",
+                "MobileAgent-09: Idle"};
+
+        AgentPool pool = new AgentPool(new MobileAgentFactory("emissary.core.HDMobileAgent"), 10);
+        Namespace.bind("MobileAgent-07", "PHASE.FORM.Place");
+        Namespace.bind("AgentPool", pool);
+        try (Response response = target("agents").request().get()) {
+            assertEquals(200, response.getStatus());
+            AgentsResponseEntity entity = response.readEntity(AgentsResponseEntity.class);
+            assertTrue(entity.getErrors().isEmpty());
+            assertTrue(entity.getCluster().isEmpty());
+            assertEquals("localhost:8001", entity.getLocal().getHost());
+            assertIterableEquals(Arrays.asList(expectedAgents), entity.getLocal().getAgents());
+        } finally {
+            Namespace.unbind("MobileAgent-07");
+            Namespace.unbind("AgentPool");
+        }
+    }
+
+    @Test
+    void pool() {
+        AgentPool pool = new AgentPool(new MobileAgentFactory("emissary.core.HDMobileAgent"), 10);
+        Namespace.bind("AgentPool", pool);
+        try (Response response = target("pool").request().get()) {
+            assertEquals(200, response.getStatus());
+            MapResponseEntity entity = response.readEntity(MapResponseEntity.class);
+            assertTrue(entity.getErrors().isEmpty());
+            assertFalse(entity.getResponse().isEmpty());
+            assertEquals("Poolsize active/idle: 0/10", entity.getResponse().get("localhost:8001"));
+        } finally {
+            Namespace.unbind("AgentPool");
+        }
+    }
+
+    @Test
+    void places() {
+        Namespace.bind("PickupPlace", "pickupPlace");
+        Namespace.bind("PickupClient", "pickupClient");
+        Namespace.bind("ThisWontBeAdded", "miss");
+        Namespace.bind("ProcessingPlace", "processingPlace");
+        try (Response response = target("places").request().get()) {
+            assertEquals(200, response.getStatus());
+            PlacesResponseEntity entity = response.readEntity(PlacesResponseEntity.class);
+            assertEquals("localhost:8001", entity.getLocal().getHost());
+            assertEquals(3, entity.getLocal().getPlaces().size());
+            assertEquals(Sets.newHashSet("pickupClient", "pickupPlace", "processingPlace"), entity.getLocal().getPlaces());
+            assertEquals(0, entity.getErrors().size());
+            assertTrue(CollectionUtils.isEmpty(entity.getCluster()));
+        } finally {
+            Namespace.unbind("PickupPlace");
+            Namespace.unbind("PickupClient");
+            Namespace.unbind("ThisWontBeAdded");
+            Namespace.unbind("ProcessingPlace");
+        }
+    }
+
+    @Test
+    void placesNoPlacesClientsBound() {
+        try (Response response = target("places").request().get()) {
+            assertEquals(200, response.getStatus());
+            PlacesResponseEntity entity = response.readEntity(PlacesResponseEntity.class);
+            assertEquals("localhost:8001", entity.getLocal().getHost());
+            assertEquals(0, entity.getLocal().getPlaces().size());
+            assertEquals(0, entity.getErrors().size());
+            assertTrue(CollectionUtils.isEmpty(entity.getCluster()));
+        }
+    }
+
+    @Test
+    void placesNoServerBound() {
+        Namespace.unbind("EmissaryServer");
+        try (Response response = target("places").request().get()) {
+            assertEquals(200, response.getStatus());
+            PlacesResponseEntity entity = response.readEntity(PlacesResponseEntity.class);
+            assertTrue(CollectionUtils.isEmpty(entity.getLocal().getPlaces()));
+            assertTrue(CollectionUtils.isEmpty(entity.getCluster()));
+            assertEquals(1, entity.getErrors().size());
+            assertEquals(Collections.singleton("Problem finding the emissary server or places in the namespace: Not found: EmissaryServer"),
+                    entity.getErrors());
+        }
+    }
+
+    @Test
+    void version() {
+        try (Response response = target("version").request().get()) {
+            assertEquals(200, response.getStatus());
+            MapResponseEntity entity = response.readEntity(MapResponseEntity.class);
+            assertTrue(entity.getErrors().isEmpty());
+            assertFalse(entity.getResponse().isEmpty());
+            assertEquals(new emissary.util.Version().getVersion(), entity.getResponse().get("localhost:8001"));
+        }
+    }
+
+    @Test
+    void getEnvJson() {
+        try (Response response = target("env").request().get()) {
+            assertEquals(200, response.getStatus());
+            MapResponseEntity entity = response.readEntity(MapResponseEntity.class);
+            assertTrue(entity.getErrors().isEmpty());
+            assertFalse(entity.getResponse().isEmpty());
+            assertEquals("/path/to/project/config", entity.getResponse().get("CONFIG_DIR"));
+            assertEquals("/path/to/project", entity.getResponse().get("PROJECT_BASE"));
+            assertEquals("/path/to/project/output", entity.getResponse().get("OUTPUT_ROOT"));
+            assertEquals("/path/to/project/bin", entity.getResponse().get("BIN_DIR"));
+            assertEquals("localhost", entity.getResponse().get("HOST"));
+            assertEquals("8001", entity.getResponse().get("PORT"));
+            assertEquals("https", entity.getResponse().get("SCHEME"));
+        }
+    }
+
+    @Test
+    void getEnvForBash() {
+        try (Response response = target("env.sh").request().get()) {
+            assertEquals(200, response.getStatus());
+            String entity = response.readEntity(String.class);
+            assertTrue(StringUtils.isNotEmpty(entity));
+            assertTrue(entity.contains("export CONFIG_DIR=\"/path/to/project/config\""));
+            assertTrue(entity.contains("export PROJECT_BASE=\"/path/to/project\""));
+            assertTrue(entity.contains("export OUTPUT_ROOT=\"/path/to/project/output\""));
+            assertTrue(entity.contains("export BIN_DIR=\"/path/to/project/bin\""));
+            assertTrue(entity.contains("export HOST=\"localhost\""));
+            assertTrue(entity.contains("export PORT=\"8001\""));
+            assertTrue(entity.contains("export SCHEME=\"https\""));
+        }
+    }
+
+    @Test
+    void metrics() {
+        MetricsAction metrics = new MetricsAction();
+        MetricsManager manager = mock(MetricsManager.class);
+        MetricRegistry registry = mock(MetricRegistry.class);
+
+        Counter metric = new Counter();
+        metric.inc(100);
+
+        final SortedMap<String, Metric> results = new TreeMap<>();
+        results.put("testing", metric);
+
+        when(registry.getMetrics()).thenReturn(results);
+        when(manager.getMetricRegistry()).thenReturn(registry);
+
+        Namespace.bind("MetricsManager", manager);
+        try (Response response = metrics.clusterAgents()) {
+            assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+            Map<String, Metric> metricsMap = ((MetricRegistry) response.getEntity()).getMetrics();
+            assertEquals(100, ((Counter) metricsMap.get("testing")).getCount());
+        } finally {
+            Namespace.unbind("MetricsManager");
+        }
+    }
+
+    @Test
+    void healthcheck() {
+        HealthCheckAction health = new HealthCheckAction();
+        HealthCheckRegistry registry = mock(HealthCheckRegistry.class);
+        MetricsManager manager = mock(MetricsManager.class);
+        final SortedMap<String, HealthCheck.Result> results = new TreeMap<>();
+        results.put("testing", HealthCheck.Result.healthy("Okay"));
+
+        when(manager.getHealthCheckRegistry()).thenReturn(registry);
+        when(registry.runHealthChecks()).thenReturn(results);
+
+        Namespace.bind("MetricsManager", manager);
+        try (Response response = health.clusterAgents()) {
+            assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+            assertTrue(response.getEntity().toString().contains("isHealthy=true"));
+            assertTrue(response.getEntity().toString().contains("message=Okay"));
+        } finally {
+            Namespace.unbind("MetricsManager");
+        }
+    }
+
+    @Test
+    void apiutils() throws Exception {
+
+        String PEER = "*.*.*.http://somehost:8001/DirectoryPlace";
+        Set<String> PEERS = Collections.singleton(PEER);
+
+        DirectoryPlace mockDirectory = mock(DirectoryPlace.class);
+        when(mockDirectory.getPeerDirectories()).thenReturn(PEERS);
+
+        Namespace.bind("DirectoryPlace", mockDirectory);
+        try {
+            Set<String> results = ApiUtils.lookupPeers();
+            assertEquals(PEERS, results);
+
+            String result = ApiUtils.stripPeerString(PEER);
+            assertEquals("http://somehost:8001/", result);
+
+            assertThrows(IndexOutOfBoundsException.class, () -> ApiUtils.stripPeerString("*.*.http://throwsException:8001"));
+
+            String hostAndPort = ApiUtils.getHostAndPort();
+            assertEquals("localhost:8001", hostAndPort);
+
+            Namespace.unbind("EmissaryServer");
+            result = ApiUtils.getHostAndPort();
+            assertEquals("Namespace lookup error, host unknown", result);
+
+            Namespace.unbind("DirectoryPlace");
+            assertThrows(EmissaryException.class, ApiUtils::lookupPeers);
+        } finally {
+            Namespace.unbind("DirectoryPlace");
+        }
+    }
+}
---
+++ src/test/java/emissary/server/mvc/EmissaryMvcTest.java
@@ -0,0 +1,172 @@
+package emissary.server.mvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
+
+import emissary.core.Namespace;
+import emissary.directory.DirectoryEntry;
+import emissary.directory.DirectoryEntryList;
+import emissary.directory.IDirectoryPlace;
+import emissary.util.Version;
+import org.junit.jupiter.api.Test;
+
+class EmissaryMvcTest extends EndpointTestBase {
+
+    @Test
+    void dumpDirectory() {
+        DirectoryEntryList list = new DirectoryEntryList();
+        list.add(new DirectoryEntry("TEST", "Testing", 90, 10));
+
+        IDirectoryPlace dir = mock(IDirectoryPlace.class);
+        when(dir.getEntryKeys()).thenReturn(Collections.singleton("THE_KEY"));
+        when(dir.getEntryList("THE_KEY")).thenReturn(list);
+
+        Namespace.bind("DirectoryPlace", dir);
+        // test template is src/test/resources/dump_directory.mustache
+        try (Response response = target("DumpDirectory.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("THE_KEY"));
+            assertTrue(out.contains("<td>TEST</td>"));
+            assertTrue(out.contains("<td class=\"num\">90</td>"));
+            assertTrue(out.contains("<td class=\"num\">10</td>"));
+            assertTrue(out.contains("<td class=\"num\">9090</td>"));
+            assertTrue(out.contains("<td class=\"num\">00:00:00</td>"));
+        } finally {
+            Namespace.unbind("DirectoryPlace");
+        }
+    }
+
+    @Test
+    void env() {
+        // test template is src/test/resources/environment.mustache
+        try (Response response = target("Environment.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("<h2>Properties</h2>"));
+            assertTrue(out.contains("emissary.node.name: localhost"));
+            assertTrue(out.contains("emissary.node.port: 8001"));
+            assertTrue(out.contains("emissary.node.scheme: http"));
+            assertTrue(out.contains("emissary.node.service.type: server"));
+            assertTrue(out.contains("<h2>Environment</h2>"));
+            assertTrue(out.contains("PROJECT_BASE:"));
+        }
+    }
+
+    @Test
+    void namespace() {
+        Namespace.bind("TestDirectoryPlace", new DirectoryEntry("TEST", "TESTSERVICEPLACE", "TRANSFORM", "http://localhost:8001/TestServicePlace",
+                "emissary.test.TestServicePlace", 90, 10));
+        // cannot use JettyTest because request.getContextPath() throws null pointer, so mock it
+        NamespaceAction action = new NamespaceAction();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        when(request.getContextPath()).thenReturn("/emissary");
+        try {
+            Map<String, Object> out = action.getNamespace(request);
+            assertNotNull(out);
+            assertEquals(1, out.size());
+            Object namespaceInfoSet = out.get("namespaces");
+            assertTrue(namespaceInfoSet instanceof Set);
+            AtomicBoolean found = new AtomicBoolean(false);
+            ((Set<NamespaceAction.NamespaceInfo>) namespaceInfoSet).forEach(info -> {
+                if ("TestDirectoryPlace".equals(info.key)) {
+                    assertEquals("TestDirectoryPlace", info.key);
+                    assertEquals("TEST.TESTSERVICEPLACE.TRANSFORM.http://localhost:8001/TestServicePlace$9090 (emissary.test.TestServicePlace)",
+                            info.value.toString());
+                    assertEquals("emissary.directory.DirectoryEntry", info.valueClassName);
+                    assertTrue(info.isDir);
+                    found.set(true);
+                }
+            });
+            if (!found.get()) {
+                fail("TestDirectoryPlace was not in the NamespaceInfo LinkedHashSet");
+            }
+        } finally {
+            Namespace.unbind("TestDirectoryPlace");
+        }
+    }
+
+    @Test
+    void threaddump() {
+        try (Response response = target("Threaddump.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("Emissary Version:</strong> " + new Version().getVersion()));
+            assertTrue(out.contains("JVM Version:</strong> " + System.getProperty("java.vm.version")));
+            assertTrue(out.contains("JVM Name:</strong> " + System.getProperty("java.vm.name")));
+            assertTrue(out.contains("Deadlocked Threads:"));
+            assertTrue(out.contains("Thread Dump:"));
+        }
+    }
+
+    @Test
+    void transferdirectory() {
+        DirectoryEntryList list = new DirectoryEntryList();
+        list.add(new DirectoryEntry("TEST", "TESTSERVICEPLACE", "TRANSFORM", "http://localhost:8001/TestServicePlace",
+                "emissary.test.TestServicePlace", 90, 10));
+
+        IDirectoryPlace idir = mock(IDirectoryPlace.class);
+        when(idir.getKey()).thenReturn("TEST_KEY");
+        when(idir.getEntryKeys()).thenReturn(Collections.singleton("TestDataId"));
+        when(idir.getEntryList("TestDataId")).thenReturn(list);
+
+        Namespace.bind("DirectoryPlace", idir);
+
+        try (Response response = target("TransferDirectory.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("<directory location=\"TEST_KEY\">"));
+            assertTrue(out.contains("<entryList dataid=\"TestDataId\">"));
+            assertTrue(out.contains("<key>TEST.TESTSERVICEPLACE.TRANSFORM.http://localhost:8001/TestServicePlace</key>"));
+            assertTrue(out.contains("<description>emissary.test.TestServicePlace</description>"));
+            assertTrue(out.contains("<cost>90</cost>"));
+            assertTrue(out.contains("<quality>10</quality>"));
+            assertTrue(out.contains("<expense>9090</expense>"));
+        } finally {
+            Namespace.unbind("DirectoryPlace");
+        }
+    }
+
+    @Test
+    void unpause() {
+        // test template is src/test/resources/environment.mustache
+        try (Response response = target("Unpause.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("Unpausing server..."));
+        }
+    }
+
+    @Test
+    void pause() {
+        // test template is src/test/resources/environment.mustache
+        try (Response response = target("Pause.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("Pausing server..."));
+        }
+    }
+
+    @Test
+    void shutdown() {
+        // test template is src/test/resources/environment.mustache
+        try (Response response = target("Shutdown.action").request().get()) {
+            assertEquals(200, response.getStatus());
+            String out = response.readEntity(String.class);
+            assertTrue(out.contains("Starting shutdown..."));
+        }
+    }
+}
