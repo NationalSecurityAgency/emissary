4bcbcf9cf70c44e9e62f0536431ad0cb6a9be148
Matt <19797865+dev-mlb@users.noreply.github.com>
2022-08-10 23:11:54 -0400

    UI :: bootstrap5 fixes (#277)

--- src/main/resources/public/js/emissary_nav.js
+++ src/main/resources/public/js/emissary_nav.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
         document.title = document.title + ' - ' + data.appName;
         $('header').prepend('<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark"></nav>');
         $('nav.navbar').append('<a class="navbar-brand" href="/">' + data.appName + '</a>');
-        $('nav.navbar').append('<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>');
-        $('nav.navbar').append('<div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav mr-auto"></ul><ul class="navbar-nav ml-auto"></ul></div>');
+        $('nav.navbar').append('<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>');
+        $('nav.navbar').append('<div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav me-auto"></ul><ul class="navbar-nav ms-auto"></ul></div>');
 
         $.each(data.navItems, function (display, link) {
-            $('ul.navbar-nav.mr-auto').append('<li class="nav-item"><a class="nav-link" href="'+link+'">'+display+'</a></li>');
+            $('ul.navbar-nav.me-auto').append('<li class="nav-item"><a class="nav-link" href="'+link+'">'+display+'</a></li>');
         });
 
         $.each(data.navButtons, function (display, link) {
-            $('ul.navbar-nav.mr-auto').append('<li class="nav-item"><button id="'+display+'" class="btn btn-danger navbar-btn" style="margin: 2px 2px 0px 0px;">'+display+'</a></li>');
+            $('ul.navbar-nav.me-auto').append('<li class="nav-item"><button id="'+display+'" class="btn btn-danger navbar-btn" style="margin: 2px 2px 0px 0px;">'+display+'</a></li>');
             $('#' + display).on('click', function(){
                 if (confirm('Are you sure?')) {
                     window.location = link;
@@ -27,7 +27,7 @@ $(document).ready(function () {
             });
         });
 
-        $('ul.navbar-nav.ml-auto').append('<li class="nav-item"><span id="emissary-version" class="nav-link">'+data.appVersion+'</span></li>');
+        $('ul.navbar-nav.ms-auto').append('<li class="nav-item"><span id="emissary-version" class="nav-link">'+data.appVersion+'</span></li>');
 
         var url = window.location.pathname;
         $('ul.navbar-nav a[href="'+ url +'"]').parent().addClass('active');
