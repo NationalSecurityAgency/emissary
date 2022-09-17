1c0941d776e8020b5a3c2d136a0e1d7bcce7942a
dev-mlb <dev-mlb@users.noreply.github.com>
2022-09-17 12:05:28 -0400

    fix import statement

--- src/test/java/emissary/client/response/AgentsFormatterTest.java
+++ src/test/java/emissary/client/response/AgentsFormatterTest.java
@@ -1,6 +1,6 @@
 package emissary.client.response;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
