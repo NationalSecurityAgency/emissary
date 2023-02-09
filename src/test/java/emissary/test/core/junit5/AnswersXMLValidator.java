4d042b04e6e19967aa31cfa0214dc8cef37ead77
Andrew Pagan <andrew.pagan@sentinel-corp.com>
2023-02-09 21:48:43 +0000

    WIP

--- src/test/java/emissary/test/core/junit5/AnswersXMLValidator.java
+++ src/test/java/emissary/test/core/junit5/AnswersXMLValidator.java
@@ -1,36 +1,34 @@
 package emissary.test.core.junit5;
 
-import emissary.config.Configurator;
-
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class AnswersXMLValidator {
+    private static final Logger logger = LoggerFactory.getLogger(AnswersXMLValidator.class);
     private static final List<String> validFirstLevelXMLFields = Arrays.asList("setup", "answers");
     private static final List<String> validSetupFields = Arrays.asList("classification", "fileType", "initialForm", "meta");
     private static final List<String> validAnswersFields =
-            Arrays.asList("att", "altView", "classification", "currentForm", "currentFormSize", "data", "dataLength", "extract", "extractCount",
-                    "fileType", "meta", "nometa",
-                    "noview", "numAttachments", "shortName", "view");
+            Arrays.asList("att", "altView", "classification", "currentForm", "currentFormSize", "data", "dataLength",
+                    "extract", "extractCount", "fileType", "meta", "nometa", "noview", "numAttachments", "shortName", "view");
     private static final List<String> validInnerInnerFields = Arrays.asList("name", "value");
+    private static final List<String> validExtractFields =
+            Arrays.asList("currentForm", "currentFormSize", "data", "dataLength", "fileType", "shortName", "meta");
+    private static final List<String> validAttFields =
+            Arrays.asList("currentForm", "currentFormSize", "data", "dataLength", "fileType", "shortName", "meta");
     private static final List<String> validDataAttributes = Arrays.asList("matchMode");
-    Configurator configG;
-
 
     public static void validate(Document answerDoc) {
         Element root = answerDoc.getRootElement();
-        assertEquals("result", root.getName());
+        warnIfNotTrue(root.getName().equals("result"), "Root is '" + root.getName() + "', expected 'result'.");
         List<Element> firstLevelElements = root.getChildren();
         firstLevelElements.forEach(child -> {
             String childName = child.getName();
-            assertTrue(validFirstLevelXMLFields.contains(childName),
+            warnIfNotTrue(validFirstLevelXMLFields.contains(childName),
                     "'" + childName + "' is not in list of valid first level XML fields:\n" + validFirstLevelXMLFields);
             switch (childName) {
                 case "answers":
@@ -40,7 +38,7 @@ public class AnswersXMLValidator {
                     validateInnerChild(child, childName, validSetupFields);
                     break;
                 default:
-                    fail("Invalid child '" + childName + "'; this should never happen");
+                    warnIfNotTrue(false, "Invalid child '" + childName + "'; this should never happen");
             }
         });
     }
@@ -48,23 +46,45 @@ public class AnswersXMLValidator {
     private static void validateInnerChild(Element child, String childName, List<String> validFields) {
         child.getChildren().forEach(innerChild -> {
             final String innerChildName = innerChild.getName();
-            final String innerChildNameTruncated = Character.isDigit(innerChildName.charAt(innerChildName.length() - 1))
-                    ? innerChildName.substring(0, innerChildName.length() - 1)
+            final String innerChildNameTruncated = innerChildName.contains("att") || innerChildName.contains("extract")
+                    ? innerChildName.replaceAll("\\d", "")
                     : innerChildName;
-            assertTrue(validFields.contains(innerChildNameTruncated),
+            warnIfNotTrue(validFields.contains(innerChildNameTruncated),
                     "'" + innerChildName + "' is not in list of valid '" + childName + "' fields:\n" + validFields);
-            switch (innerChildName) {
+            List<Element> innerInnerChildren = innerChild.getChildren();
+            switch (innerChildNameTruncated) {
                 case "data":
-                    innerChild.getAttributes().forEach(attribute -> assertTrue(validDataAttributes.contains(attribute.getName()),
+                    innerChild.getAttributes().forEach(attribute -> warnIfNotTrue(validDataAttributes.contains(attribute.getName()),
                             "'" + attribute.getName() + "' is not in list of valid data attributes:\n" + validDataAttributes));
-                default:
-                    List<Element> innerInnerChildren = innerChild.getChildren();
+                    break;
+
+                case "extract":
                     innerInnerChildren.forEach(innerInnerChild -> {
                         String innerInnerChildName = innerInnerChild.getName();
-                        assertTrue(validInnerInnerFields.contains(innerInnerChildName),
+                        warnIfNotTrue(validExtractFields.contains(innerInnerChildName),
+                                "'" + innerInnerChildName + "' is not in list of valid '" + innerChildName + "' fields:\n" + validExtractFields);
+                    });
+                    break;
+                case "att":
+                    innerInnerChildren.forEach(innerInnerChild -> {
+                        String innerInnerChildName = innerInnerChild.getName();
+                        warnIfNotTrue(validAttFields.contains(innerInnerChildName),
+                                "'" + innerInnerChildName + "' is not in list of valid '" + innerChildName + "' fields:\n" + validAttFields);
+                    });
+                    break;
+                default:
+                    innerInnerChildren.forEach(innerInnerChild -> {
+                        String innerInnerChildName = innerInnerChild.getName();
+                        warnIfNotTrue(validInnerInnerFields.contains(innerInnerChildName),
                                 "'" + innerInnerChildName + "' is not in list of valid '" + innerChildName + "' fields:\n" + validInnerInnerFields);
                     });
             }
         });
+    }
+
+    private static void warnIfNotTrue(boolean condition, String message) {
+        if (!condition) {
+            logger.warn(message);
+        }
     }
 }
