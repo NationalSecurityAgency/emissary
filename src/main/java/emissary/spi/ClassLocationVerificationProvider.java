4bdc452991be194fab8fa4424979df6af2489f05
fbruton <19573915+fbruton@users.noreply.github.com>
2023-03-30 15:21:48 -0400

    Verify class locations during initialization (#445)

---
+++ src/main/java/emissary/spi/ClassLocationVerificationProvider.java
@@ -0,0 +1,62 @@
+package emissary.spi;
+
+import emissary.config.ConfigUtil;
+import emissary.config.Configurator;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ClassLocationVerificationProvider implements InitializationProvider {
+    private static final Logger logger = LoggerFactory.getLogger(ClassLocationVerificationProvider.class);
+
+    @Override
+    public void initialize() {
+
+        try {
+            Configurator configG = ConfigUtil.getConfigInfo(ClassLocationVerificationProvider.class);
+            Map<String, String> classNameLocationMap = configG.findStringMatchMap("CLASS_NAME_", true);
+            classNameLocationMap.forEach(ClassLocationVerificationProvider::verify);
+        } catch (IOException e) {
+            logger.debug("Failed to load config", e);
+        }
+
+    }
+
+    protected static boolean verify(String className, String expectedClassLoc) {
+        boolean success = true;
+        try {
+            Class<?> clazz = Class.forName(className);
+            String classLocPath = clazz.getProtectionDomain().getCodeSource().getLocation().getFile();
+            String classLocName = FilenameUtils.getName(classLocPath);
+
+            if (ifArchivedFileNameDoesNotStartsWithExpectedLocation(expectedClassLoc, classLocPath, classLocName)
+                    || ifWorkingDirDoesNotContainExpectedLocation(expectedClassLoc, classLocName)) {
+                logError(className, expectedClassLoc, classLocPath);
+                success = false;
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Failed to find class: {}", className);
+            success = false;
+        }
+
+        return success;
+    }
+
+    private static boolean ifWorkingDirDoesNotContainExpectedLocation(String expectedLocation, String locationName) {
+        return StringUtils.isNotBlank(locationName) && !StringUtils.startsWith(locationName, expectedLocation);
+    }
+
+    private static boolean ifArchivedFileNameDoesNotStartsWithExpectedLocation(String expectedLocation, String location, String locationName) {
+        return StringUtils.isBlank(locationName) && !StringUtils.contains(location, expectedLocation);
+    }
+
+    private static void logError(String className, String expectedLocation, String location) {
+        logger.error("Failed to load expected class: {}, actual location: {}, expected location: {}", className, location,
+                expectedLocation);
+    }
+}
---
+++ src/test/java/emissary/spi/ClassLocationVerificationProviderTest.java
@@ -0,0 +1,30 @@
+package emissary.spi;
+
+import emissary.Emissary;
+import emissary.test.core.junit5.UnitTest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ClassLocationVerificationProviderTest extends UnitTest {
+    @Test
+    void testVerifyClassInWorkingDirectory() {
+        String emissaryClassName = Emissary.class.getName();
+        boolean status = ClassLocationVerificationProvider.verify(emissaryClassName, "emissary");
+        Assertions.assertTrue(status);
+
+        status = ClassLocationVerificationProvider.verify(emissaryClassName, "doesnotexist");
+        Assertions.assertFalse(status);
+    }
+
+    @Test
+    void testVerifyClassInJar() {
+        String javaClassName = StringUtils.class.getName();
+        boolean status = ClassLocationVerificationProvider.verify(javaClassName, "commons-lang");
+        Assertions.assertTrue(status);
+
+        status = ClassLocationVerificationProvider.verify(javaClassName, "doesnotexist");
+        Assertions.assertFalse(status);
+    }
+}
