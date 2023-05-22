c8f3973479c2e7c94c69a708f0836dd1ab5db474
Sam Bishop <SamBishop911@yahoo.com>
2023-05-22 13:09:48 -0400

    remove * imports

--- src/test/java/emissary/util/PayloadUtilTest.java
+++ src/test/java/emissary/util/PayloadUtilTest.java
@@ -17,7 +17,9 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PayloadUtilTest extends UnitTest {
 
