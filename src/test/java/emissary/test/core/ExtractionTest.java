d6598cfad1193956f2f9f48771d8535b1e4120af
Sam Bishop <SamBishop911@yahoo.com>
2023-06-02 09:14:28 -0400

    fix * import

--- src/test/java/emissary/test/core/ExtractionTest.java
+++ src/test/java/emissary/test/core/ExtractionTest.java
@@ -32,7 +32,12 @@ import java.util.Collections;
 import java.util.List;
 import javax.xml.bind.DatatypeConverter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(Parameterized.class)
 public abstract class ExtractionTest extends UnitTest {
