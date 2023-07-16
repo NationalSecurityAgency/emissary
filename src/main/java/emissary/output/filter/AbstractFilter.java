2f6bc5388f78ae4a929dcd99839cb63133f9af96
dev-mlb <19797865+dev-mlb@users.noreply.github.com>
2023-07-16 07:38:19 -0400

    rename to denylist

--- src/main/java/emissary/output/filter/AbstractFilter.java
+++ src/main/java/emissary/output/filter/AbstractFilter.java
@@ -80,7 +80,7 @@ public abstract class AbstractFilter implements IDropOffFilter {
     public static final String METADATA_VIEW = "." + METADATA_VIEW_NAME;
 
     /* alternate views to NOT output if only a file type/form is specified */
-    protected Set<String> ignorelist = Collections.emptySet();
+    protected Set<String> denylist = Collections.emptySet();
 
     protected DropOffUtil dropOffUtil = null;
 
@@ -167,8 +167,8 @@ public abstract class AbstractFilter implements IDropOffFilter {
         if (config != null) {
             this.outputTypes = config.findEntriesAsSet("OUTPUT_TYPE");
             this.logger.debug("Loaded {} output types for filter {}", this.outputTypes.size(), this.outputTypes);
-            this.ignorelist = config.findEntriesAsSet("IGNORELIST");
-            this.logger.debug("Loaded {} ignorelist types for filter {}", this.ignorelist.size(), this.ignorelist);
+            this.denylist = config.findEntriesAsSet("DENYLIST");
+            this.logger.debug("Loaded {} ignorelist types for filter {}", this.denylist.size(), this.denylist);
         } else {
             this.logger.debug("InitializeCustom has null filter config");
         }
@@ -445,8 +445,8 @@ public abstract class AbstractFilter implements IDropOffFilter {
         final String fileType = DropOffUtil.getFileType(d);
         final String currentForm = d.currentForm();
 
-        // skip over ignorelisted alt views
-        if (this.ignorelist.contains(viewName) || this.ignorelist.contains(fileType + "." + viewName)) {
+        // skip over denylisted alt views
+        if (this.denylist.contains(viewName) || this.denylist.contains(fileType + "." + viewName)) {
             return checkTypes;
         }
 
