4898f203c86a6de770b6bcecff2fbaea5a325baa
Amanda <amanda.pyryt@peraton.com>
2023-08-03 18:13:45 +0000

    Removing unused method

--- src/main/java/emissary/util/ObjectTracing.java
+++ src/main/java/emissary/util/ObjectTracing.java
@@ -1,11 +1,9 @@
 package emissary.util;
 
-import emissary.config.Configurator;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -15,28 +13,9 @@ public class ObjectTracing {
 
     private ObjectTracing() {}
 
-    private static List<String> fieldNames;
+    private static final List<String> fieldNames = new ArrayList<>();
 
     protected static final Logger logger = LoggerFactory.getLogger(ObjectTracing.class);
-
-
-    public static synchronized void configure() {
-        Configurator configG;
-        try {
-            configG = emissary.config.ConfigUtil.getConfigInfo(ObjectTracing.class);
-        } catch (IOException e) {
-            logger.error("Cannot open default config file", e);
-            return;
-        }
-
-        fieldNames = configG.findEntries("FIELD_NAME");
-
-        StringBuilder fieldNameString = new StringBuilder();
-        for (String name : fieldNames) {
-            fieldNameString.append(name).append(" ");
-        }
-        logger.info("The fields configured to be logged in ObjectTracing are: {}", fieldNameString);
-    }
 
     public static void setUpFieldNames(List<String> newFieldNames) {
         fieldNames.addAll(newFieldNames);
