11eb6c2b21f8b3814e7fd799b43b1a72d793c039
Amanda <amanda.pyryt@peraton.com>
2023-08-17 18:59:32 +0000

    Handing error case in setting up ObjectTracingLogbackAppender

--- src/main/java/emissary/util/ObjectTracingLogbackAppender.java
+++ src/main/java/emissary/util/ObjectTracingLogbackAppender.java
@@ -4,6 +4,8 @@ import emissary.config.ConfigUtil;
 import emissary.config.Configurator;
 
 import ch.qos.logback.core.rolling.RollingFileAppender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,13 +14,14 @@ public class ObjectTracingLogbackAppender<E> extends RollingFileAppender<E> {
     private int rolloverTime = 15;
     private boolean isConfigured = false;
 
+    protected static final Logger logger = LoggerFactory.getLogger(ObjectTracingLogbackAppender.class);
+
     public void configure() {
         try {
             Configurator configG = ConfigUtil.getConfigInfo(getClass());
             rolloverTime = configG.findIntEntry("ROLLOVER_TIME_MINUTES", rolloverTime);
         } catch (IOException e) {
-            // TODO -- how to handle this?
-            throw new RuntimeException(e);
+            logger.error("Could not read from the config for the ObjectTracingLogbackAppender ", e);
         }
     }
 
