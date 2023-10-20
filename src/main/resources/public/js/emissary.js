7f94ac0fb0da6bb509d0ea01e0688071c3c0bbba
Sam Bishop <SamBishop911@yahoo.com>
2023-10-20 14:08:24 -0400

    Adding search ability in directory action in API

--- src/main/resources/public/js/emissary.js
+++ src/main/resources/public/js/emissary.js
@@ -29,3 +29,33 @@ function doPost( url, messageHolderId) {
             $("#" + messageHolderId).append("request failed!");
         });
 }
+
+function searchTable() {
+    var input, filter, table, tbody, tr, td, i, txtValue;
+    var result = 0;
+      input = document.getElementById("searchInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("tableToSearch");
+      tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                if (tr[i - 1].getElementsByTagName("td").length == 1) {
+                    tr[i - 1].style.display = "";
+                }
+                result += 1;
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+    if (result == 0) {
+        document.getElementById("empty-message").style.display = "";
+    } else {
+        document.getElementById("empty-message").style.display = "none";
+    }
+}
