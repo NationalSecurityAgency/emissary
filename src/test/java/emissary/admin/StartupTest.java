5794d03d5b79b19849c0b2da87daf844ca22ecbd
Sam Bishop <48037840+sambish5@users.noreply.github.com>
2023-11-07 09:53:00 -0500

    addressing unexpected place startups (#598)

--- src/test/java/emissary/admin/StartupTest.java
+++ src/test/java/emissary/admin/StartupTest.java
@@ -1,5 +1,7 @@
 package emissary.admin;
 
+import emissary.core.Namespace;
+import emissary.directory.DirectoryPlace;
 import emissary.directory.EmissaryNode;
 import emissary.pickup.file.FilePickUpClient;
 import emissary.pickup.file.FilePickUpPlace;
@@ -7,18 +9,22 @@ import emissary.place.CoordinationPlace;
 import emissary.place.sample.DelayPlace;
 import emissary.place.sample.DevNullPlace;
 import emissary.test.core.junit5.UnitTest;
+import emissary.util.io.ResourceReader;
 
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
 
 class StartupTest extends UnitTest {
-
     @Test
     void testSortPlaces() throws IOException {
         List<String> somePlaces = new ArrayList<>();
@@ -43,4 +49,64 @@ class StartupTest extends UnitTest {
                 places);
     }
 
+    @Test
+    void testInvisPlaceStart() throws IOException {
+        // setup node, startup, and DirectoryPlace
+        EmissaryNode node = new EmissaryNode();
+        Startup startup = new Startup(node.getNodeConfigurator(), node);
+        String location = "http://" + node.getNodeName() + ":" + node.getNodePort();
+        dirStartUp();
+
+        // test if place is already started before startup
+        startup.activeDirPlaces.add(location + "/PlaceAlreadyStartedTest");
+        startup.placeAlreadyStarted.add(location + "/PlaceAlreadyStartedTest");
+        assertTrue(startup.verifyNoInvisiblePlacesStarted());
+        startup.activeDirPlaces.clear();
+        startup.placeAlreadyStarted.clear();
+
+        // test if place is started up normally and is active dir
+        startup.places.put(location, DevNullPlace.class.getSimpleName());
+        startup.activeDirPlaces.add(DevNullPlace.class.getSimpleName());
+        assertTrue(startup.verifyNoInvisiblePlacesStarted());
+        startup.activeDirPlaces.clear();
+        startup.places.remove(location, DevNullPlace.class.getSimpleName());
+
+        // test unannounced place with active dir
+        startup.activeDirPlaces.add(location + "/PlaceStartUnannouncedTest");
+        assertFalse(startup.verifyNoInvisiblePlacesStarted());
+        startup.activeDirPlaces.clear();
+
+        // teardown
+        dirTeardown();
+    }
+
+    private DirectoryPlace master = null;
+    private DirectoryPlace client = null;
+
+    public void dirStartUp() throws IOException {
+        // master directory
+        InputStream configStream = new ResourceReader().getConfigDataAsStream(this);
+
+        String masterloc = "http://localhost:8001/TestMasterDirectoryPlace";
+        this.master = spy(new DirectoryPlace(configStream, masterloc, new EmissaryNode()));
+        configStream.close();
+        Namespace.bind(masterloc, this.master);
+
+        // non-master directory
+        configStream = new ResourceReader().getConfigDataAsStream(this);
+        String clientloc = "http://localhost:8001/DirectoryPlace";
+        this.client = spy(new DirectoryPlace(configStream, this.master.getDirectoryEntry().getKey(), clientloc, new EmissaryNode()));
+        configStream.close();
+        Namespace.bind(clientloc, this.client);
+    }
+
+    public void dirTeardown() {
+        this.master.shutDown();
+        this.master = null;
+        this.client.shutDown();
+        this.client = null;
+        for (String s : Namespace.keySet()) {
+            Namespace.unbind(s);
+        }
+    }
 }
