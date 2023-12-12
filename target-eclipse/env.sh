607ed8edcec77ed467fc59b3a64b1b6a7deccee7
Connor Koehler <connor@connortech.me>
2023-12-11 19:49:36 -0500

    latest site after emissary 8.0.0-M10
    Signed-off-by: Connor Koehler <connor@connortech.me>

---
+++ target-eclipse/env.sh
@@ -0,0 +1,8 @@
+# Used for testing, paths should be maven filtered
+BIN_DIR="/Users/connor/Projects/work/emissary/target-eclipse/bin"
+CONFIG_DIR="/Users/connor/Projects/work/emissary/target-eclipse/config"
+HOST="localhost"
+OUTPUT_ROOT="/Users/connor/Projects/work/emissary/target-eclipse/localoutput"
+PORT="8001"
+PROJECT_BASE="/Users/connor/Projects/work/emissary/target-eclipse"
+SCHEME="http"
