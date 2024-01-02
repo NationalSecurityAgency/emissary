ca3be4907a515d19a382c19b0eaf1dbad051572e
Alfred Arsenault awarse2 <awarse2@ip-10-113-15-50.evoforge.org>
2024-01-02 16:10:28 +0000

    Added additional unit test to prove no exceptions are now thrown

--- src/test/java/emissary/id/UnixFilePlaceTest.java
+++ src/test/java/emissary/id/UnixFilePlaceTest.java
@@ -1,12 +1,24 @@
 package emissary.id;
 
+import emissary.core.DataObjectFactory;
+import emissary.core.Form;
+import emissary.core.IBaseDataObject;
 import emissary.place.IServiceProviderPlace;
 import emissary.test.core.junit5.IdentificationTest;
+import emissary.test.core.junit5.LogbackTester;
+import emissary.util.io.ResourceReader;
 
+import ch.qos.logback.classic.Level;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 class UnixFilePlaceTest extends IdentificationTest {
 
@@ -14,8 +26,36 @@ class UnixFilePlaceTest extends IdentificationTest {
         return getMyTestParameterFiles(UnixFilePlaceTest.class);
     }
 
+
     @Override
     public IServiceProviderPlace createPlace() throws IOException {
         return new UnixFilePlace();
     }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void testDisabled(String resource) throws IOException {
+
+        if (resource.equals("emissary/id/UnixFilePlaceTest/UNKNOWN@2.dat")) {
+            try (LogbackTester logbackTester = new LogbackTester(UnixFilePlace.class.getName())) {
+                try (InputStream doc = new ResourceReader().getResourceAsStream(resource)) {
+                    byte[] data = IOUtils.toByteArray(doc);
+                    IBaseDataObject payload = DataObjectFactory.getInstance(data, resource, Form.UNKNOWN);
+                    processPreHook(payload, resource);
+                    place.agentProcessHeavyDuty(payload);
+                    processPostHook(payload, resource);
+                } catch (Exception ex) {
+                    logger.error("Error running test {}", resource, ex);
+                    fail("Cannot run test " + resource, ex);
+                }
+                // the initialized values passed in here indicate that no exception has been thrown
+                logbackTester.checkLogList(new Level[0], new String[0], new boolean[0]);
+            }
+        }
+
+        logger.debug("Running {} test on resource {}", place.getClass().getName(), resource);
+
+
+    }
+
 }
