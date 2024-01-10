42880883b0e85092960175144998780441802523
James Cover jdcove2 <jdcove2@ip-10-113-15-128.evoforge.org>
2024-01-10 20:40:34 +0000

    Add ComparisonPlace.

---
+++ src/main/java/emissary/place/ComparisonPlace.java
@@ -0,0 +1,142 @@
+package emissary.place;
+
+import emissary.core.DiffCheckConfiguration;
+import emissary.core.IBaseDataObject;
+import emissary.core.IBaseDataObjectHelper;
+import emissary.core.ResourceException;
+import emissary.util.PlaceComparisonHelper;
+
+import org.apache.commons.lang3.Validate;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This place takes two other places, givens them exactly the same IBDO and compares the output.
+ * <p>
+ * The configuration file must contain the five properties defined by the five constants in this class.
+ */
+public class ComparisonPlace extends ServiceProviderPlace {
+    /**
+     * The full pathname for the class for the first class.
+     */
+    public static final String PLACE_A_CLASSNAME = "PLACE_A_CLASSNAME";
+    /**
+     * The full pathname for the configuration for the first class.
+     */
+    public static final String PLACE_A_CONFIGNAME = "PLACE_A_CONFIGNAME";
+    /**
+     * The full pathname for the class for the second class.
+     */
+    public static final String PLACE_B_CLASSNAME = "PLACE_B_CLASSNAME";
+    /**
+     * The full pathname for the configuration for the second class.
+     */
+    public static final String PLACE_B_CONFIGNAME = "PLACE_B_CONFIGNAME";
+    /**
+     * An identifier to be added to the log message.
+     */
+    public static final String LOGGING_IDENTIFIER = "LOGGING_IDENTIFIER";
+
+    private static final String NOT_NULL_STRING = " == null not allowed!";
+    private static final String PROCESS_PROCESSHD_MSG = "Mixed process/processHeavyDuty not allowed!";
+
+    private final ServiceProviderPlace placeA;
+    private final ServiceProviderPlace placeB;
+    private final String loggingIdentifier;
+
+    public ComparisonPlace(String configFile, String theDir, String thePlaceLocation) throws IOException {
+        super(configFile, theDir, thePlaceLocation);
+
+        final String placeAClassName = configG.findStringEntry(PLACE_A_CLASSNAME);
+        final String placeAConfigName = configG.findStringEntry(PLACE_A_CONFIGNAME);
+        final String placeBClassName = configG.findStringEntry(PLACE_B_CLASSNAME);
+        final String placeBConfigName = configG.findStringEntry(PLACE_B_CONFIGNAME);
+
+        loggingIdentifier = configG.findStringEntry(LOGGING_IDENTIFIER);
+
+        Validate.notNull(placeAClassName, PLACE_A_CLASSNAME + NOT_NULL_STRING);
+        Validate.notNull(placeAConfigName, PLACE_A_CONFIGNAME + NOT_NULL_STRING);
+        Validate.notNull(placeBClassName, PLACE_B_CLASSNAME + NOT_NULL_STRING);
+        Validate.notNull(placeBConfigName, PLACE_B_CONFIGNAME + NOT_NULL_STRING);
+        Validate.notNull(loggingIdentifier, LOGGING_IDENTIFIER + NOT_NULL_STRING);
+
+        placeA = createPlace(placeAClassName, placeAConfigName);
+        placeB = createPlace(placeBClassName, placeBConfigName);
+
+        Validate.isTrue(placeA.processMethodImplemented == placeB.processMethodImplemented, PROCESS_PROCESSHD_MSG);
+    }
+
+    @Override
+    public List<IBaseDataObject> processHeavyDuty(IBaseDataObject ibdoA) throws ResourceException {
+        final IBaseDataObject ibdoB = IBaseDataObjectHelper.clone(ibdoA, true);
+        final List<IBaseDataObject> attachmentsA;
+        final List<IBaseDataObject> attachmentsB;
+
+        if (placeA.processMethodImplemented) {
+            placeA.process(ibdoA);
+            placeB.process(ibdoB);
+
+            attachmentsA = Collections.emptyList();
+            attachmentsB = Collections.emptyList();
+        } else {
+            attachmentsA = placeA.processHeavyDuty(ibdoA);
+            attachmentsB = placeB.processHeavyDuty(ibdoB);
+        }
+
+        final String differences = checkDifferencesHook(ibdoA, ibdoB, attachmentsA, attachmentsB, loggingIdentifier);
+
+        if (differences != null) {
+            logDifferencesHook(differences);
+        }
+
+        return attachmentsA;
+    }
+
+    /**
+     * This method checks for the differences in the output between the two places and can be overridden for custom
+     * behaviour.
+     * 
+     * @param ibdoA the IBDO from the first place.
+     * @param ibdoB the IBDO from the second place.
+     * @param attachmentsA the list of attachments from the first place.
+     * @param attachmentsB the list of attachments from the second place.
+     * @param loggingIdentifier the identifier to be added to the message to be logged.
+     * @return the differences to be logger or null if there are no differences.
+     */
+    protected String checkDifferencesHook(final IBaseDataObject ibdoA, final IBaseDataObject ibdoB, final List<IBaseDataObject> attachmentsA,
+            final List<IBaseDataObject> attachmentsB, final String loggingIdentifier) {
+        return PlaceComparisonHelper.checkDifferences(ibdoA, ibdoB, attachmentsA, attachmentsB, loggingIdentifier,
+                DiffCheckConfiguration.onlyCheckData());
+    }
+
+    /**
+     * This method logs the differences in the output between the two places and can be overriden for custom behaviour.
+     * 
+     * @param differences to be logged.
+     */
+    protected void logDifferencesHook(final String differences) {
+        logger.info(differences);
+    }
+
+    /**
+     * This method creates a place given a class name and configuration file.
+     * 
+     * @param className of the place.
+     * @param configName of the place.
+     * @return the place
+     * @throws IOException if anything goes wrong.
+     */
+    public static final ServiceProviderPlace createPlace(final String className, final String configName) throws IOException {
+        try {
+            final Class<?> placeAClass = Class.forName(className);
+            final Constructor<?> placeAConstructor = placeAClass.getConstructor(String.class, String.class, String.class);
+
+            return (ServiceProviderPlace) placeAConstructor.newInstance(null, null, configName);
+        } catch (Exception e) {
+            throw new IOException("Unable to create " + className, e);
+        }
+    }
+}
---
+++ src/test/java/emissary/place/ComparisonPlaceTest.java
@@ -0,0 +1,110 @@
+package emissary.place;
+
+import emissary.core.BaseDataObject;
+import emissary.core.IBaseDataObject;
+import emissary.test.core.junit5.LogbackTester;
+import emissary.test.core.junit5.LogbackTester.SimplifiedLogEvent;
+import emissary.test.core.junit5.UnitTest;
+
+import ch.qos.logback.classic.Level;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ComparisonPlaceTest extends UnitTest {
+    private static final String TEST_FILENAME_FORM = "TEST";
+    private static final String BAD_PLACE_A_CLASS_NAME = "emissary.place.ComparisonPlaceTest.BadPlaceAClassName";
+    private static final String BAD_PLACE_A_CONFIG_NAME = "emissary.place.ComparisonPlaceTest.BadPlaceAConfigName";
+    private static final String BAD_PLACE_B_CLASS_NAME = "emissary.place.ComparisonPlaceTest.BadPlaceBClassName";
+    private static final String BAD_PLACE_B_CONFIG_NAME = "emissary.place.ComparisonPlaceTest.BadPlaceBConfigName";
+    private static final String MISSING_LOGGING_IDENTIFIER = "emissary.place.ComparisonPlaceTest.MissingLoggingIdentifier";
+    private static final String MISSING_PLACE_A_CLASS_NAME = "emissary.place.ComparisonPlaceTest.MissingPlaceAClassName";
+    private static final String MISSING_PLACE_A_CONFIG_NAME = "emissary.place.ComparisonPlaceTest.MissingPlaceAConfigName";
+    private static final String MISSING_PLACE_B_CLASS_NAME = "emissary.place.ComparisonPlaceTest.MissingPlaceBClassName";
+    private static final String MISSING_PLACE_B_CONFIG_NAME = "emissary.place.ComparisonPlaceTest.MissingPlaceBConfigName";
+    private static final String PROCESS_PLACE_PROCESSHD_PLACE = "emissary.place.ComparisonPlaceTest.ProcessPlaceProcessHDPlace";
+    private static final String PROCESS_PLACE_NO_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessPlaceNoChanges";
+    private static final String PROCESS_PLACE_A_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessPlaceAChanges";
+    private static final String PROCESS_PLACE_B_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessPlaceBChanges";
+    private static final String PROCESSHD_PLACE_NO_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessHDPlaceNoChanges";
+    private static final String PROCESSHD_PLACE_A_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessHDPlaceAChanges";
+    private static final String PROCESSHD_PLACE_B_CHANGES = "emissary.place.ComparisonPlaceTest.ProcessHDPlaceBChanges";
+
+    @Test
+    void testConfiguration() throws Exception {
+        assertThrows(NullPointerException.class, () -> new ComparisonPlace(null, null, MISSING_LOGGING_IDENTIFIER));
+        assertThrows(NullPointerException.class, () -> new ComparisonPlace(null, null, MISSING_PLACE_A_CLASS_NAME));
+        assertThrows(NullPointerException.class, () -> new ComparisonPlace(null, null, MISSING_PLACE_A_CONFIG_NAME));
+        assertThrows(NullPointerException.class, () -> new ComparisonPlace(null, null, MISSING_PLACE_B_CLASS_NAME));
+        assertThrows(NullPointerException.class, () -> new ComparisonPlace(null, null, MISSING_PLACE_B_CONFIG_NAME));
+
+        assertThrows(IOException.class, () -> new ComparisonPlace(null, null, BAD_PLACE_A_CLASS_NAME));
+        assertThrows(IOException.class, () -> new ComparisonPlace(null, null, BAD_PLACE_A_CONFIG_NAME));
+        assertThrows(IOException.class, () -> new ComparisonPlace(null, null, BAD_PLACE_B_CLASS_NAME));
+        assertThrows(IOException.class, () -> new ComparisonPlace(null, null, BAD_PLACE_B_CONFIG_NAME));
+
+        assertThrows(IllegalArgumentException.class, () -> new ComparisonPlace(null, null, PROCESS_PLACE_PROCESSHD_PLACE));
+    }
+
+    @Test
+    void testProcessPlaceNoChanges() throws Exception {
+        testComparisonPlace(PROCESS_PLACE_NO_CHANGES, null);
+    }
+
+    @Test
+    void testProcessPlaceAChanges() throws Exception {
+        final String logMessage = "COMPARISONPLACETEST: PDiff: parameters are not equal-Differing Keys: [] : [KEY]";
+
+        testComparisonPlace(PROCESS_PLACE_A_CHANGES, logMessage);
+    }
+
+    @Test
+    void testProcessPlaceBChanges() throws Exception {
+        final String logMessage = "COMPARISONPLACETEST: PDiff: parameters are not equal-Differing Keys: [KEY] : []";
+
+        testComparisonPlace(PROCESS_PLACE_B_CHANGES, logMessage);
+    }
+
+    @Test
+    void testProcessHDPlaceNoChanges() throws Exception {
+        testComparisonPlace(PROCESSHD_PLACE_NO_CHANGES, null);
+    }
+
+    @Test
+    void testProcessHDPlaceAChanges() throws Exception {
+        final String logMessage = "COMPARISONPLACETEST: PDiff: parameters are not equal-Differing Keys: [] : [KEY]\n" +
+                "COMPARISONPLACETEST: CDiff: COMPARISONPLACETEST : 0 : parameters are not equal-Differing Keys: [] : [KEY]";
+
+        testComparisonPlace(PROCESSHD_PLACE_A_CHANGES, logMessage);
+    }
+
+    @Test
+    void testProcessHDPlaceBChanges() throws Exception {
+        final String logMessage = "COMPARISONPLACETEST: PDiff: parameters are not equal-Differing Keys: [KEY] : []\n" +
+                "COMPARISONPLACETEST: CDiff: COMPARISONPLACETEST : 0 : parameters are not equal-Differing Keys: [KEY] : []";
+
+        testComparisonPlace(PROCESSHD_PLACE_B_CHANGES, logMessage);
+    }
+
+    private static void testComparisonPlace(final String configuration, final String logMessage) throws Exception {
+        try (LogbackTester logbackTester = new LogbackTester(ComparisonPlace.class.getName())) {
+            final ComparisonPlace comparisonPlace = new ComparisonPlace(null, null, configuration);
+            final byte[] data = configuration.getBytes(StandardCharsets.UTF_8);
+            final IBaseDataObject ibdo = new BaseDataObject(data, TEST_FILENAME_FORM, TEST_FILENAME_FORM);
+            final List<SimplifiedLogEvent> legEvents = new ArrayList<>();
+
+            if (logMessage != null) {
+                legEvents.add(new SimplifiedLogEvent(Level.INFO, logMessage, null));
+            }
+
+            comparisonPlace.processHeavyDuty(ibdo);
+
+            logbackTester.checkLogList(legEvents);
+        }
+    }
+}
