ac4cfcdb7e1dcb01d8b89d3179163ed3e398c77d
James Cover jdcove2 <jdcove2@ip-10-113-15-128.evoforge.org>
2024-02-26 22:02:31 +0000

    Have DropOffPlace stop calling IBDO.data()/IBDO.dataLength().

--- src/main/java/emissary/output/DropOffPlace.java
+++ src/main/java/emissary/output/DropOffPlace.java
@@ -225,11 +225,11 @@ public class DropOffPlace extends ServiceProviderPlace implements EmptyFormPlace
             final IBaseDataObject tld = payloadList.get(0);
 
 
-            if (outputCompletionPayloadSize && tld.data() != null) {
+            if (outputCompletionPayloadSize && tld.getChannelFactory() != null) {
                 logger.info(
                         "Finished DropOff for object {}, with external id: {}, with total processing time: {}ms, with filetype: {}, payload size: {} bytes",
                         tld.getInternalId(), this.dropOffUtil.getBestId(tld, tld), (new Date().getTime() - tld.getCreationTimestamp().getTime()),
-                        tld.getFileType(), tld.dataLength());
+                        tld.getFileType(), tld.getChannelSize());
             } else {
                 logger.info("Finished DropOff for object {}, with external id: {}, with total processing time: {}ms, with filetype: {}",
                         tld.getInternalId(), this.dropOffUtil.getBestId(tld, tld), (new Date().getTime() - tld.getCreationTimestamp().getTime()),
