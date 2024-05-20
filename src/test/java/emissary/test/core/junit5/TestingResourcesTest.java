dc62465f885410e17392447354aec1b90284a970
Sam Bishop <SamBishop911@yahoo.com>
2024-05-20 15:51:38 -0400

    adding in TestingResourcesTest

---
+++ src/test/java/emissary/test/core/junit5/TestingResourcesTest.java
@@ -0,0 +1,26 @@
+package emissary.test.core.junit5;
+
+import emissary.place.IServiceProviderPlace;
+import emissary.transform.HtmlEscapePlace;
+import emissary.transform.HtmlEscapePlaceTest;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+/**
+ * Explicitly meant to test data and answer files from different directories. This test uses {@link HtmlEscapePlaceTest}
+ * sample.dat with the sample.xml in {@link TestingResourcesTest} directory.
+ */
+public class TestingResourcesTest extends ExtractionTest {
+
+    public static Stream<? extends Arguments> data() {
+        return getMyTestParameterFiles(HtmlEscapePlaceTest.class, TestingResourcesTest.class);
+    }
+
+    @Override
+    public IServiceProviderPlace createPlace() throws IOException {
+        return new HtmlEscapePlace();
+    }
+}
