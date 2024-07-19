6b52dd711cea2b51ba1577554c56ca325d34981d
Jason Dahlke <jpdahlke@users.noreply.github.com>
2024-07-19 17:43:35 +0000

    errorprone :: MemberName - JDOMUtil

--- src/main/java/emissary/util/xml/SaferJDOMUtil.java
+++ src/main/java/emissary/util/xml/SaferJDOMUtil.java
@@ -14,6 +14,7 @@ import javax.annotation.Nullable;
  */
 public class SaferJDOMUtil extends AbstractJDOMUtil {
 
+    @SuppressWarnings("MemberName")
     protected static SAXBuilder createSAXBuilder() {
         SAXBuilder builder = createSAXBuilder(false);
         // This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all XML entity attacks are prevented
