10d84d2dc83cff9b34a9e3129cc6eca6462c75c0
Sam Bishop <SamBishop911@yahoo.com>
2024-07-30 12:34:34 -0400

    adding new TransformHistoryTest

--- src/test/java/emissary/core/TransformHistoryTest.java
+++ src/test/java/emissary/core/TransformHistoryTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TransformHistoryTest extends UnitTest {
+class TransformHistoryTest extends UnitTest {
 
     @Test
     void testSet() {
