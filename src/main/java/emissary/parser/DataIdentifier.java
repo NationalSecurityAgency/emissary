55b37040042f4acd24f01e4b635b6d81672a7927
Connor Koehler <83408002+cfkoehler@users.noreply.github.com>
2024-08-23 15:59:46 +0000

    revert DATA_ID_STR_SZ field change

--- src/main/java/emissary/parser/DataIdentifier.java
+++ src/main/java/emissary/parser/DataIdentifier.java
@@ -25,7 +25,7 @@ public class DataIdentifier {
     public static final String UNKNOWN_TYPE = "simple";
 
     // Size of string to test
-    protected static final int DATA_ID_STR_SZ = 100;
+    protected int DATA_ID_STR_SZ = 100;
 
     // Things we know how to identify
     protected Map<String, String> typesMap = new HashMap<>();
