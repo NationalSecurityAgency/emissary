2ba9053e63c32f6ca0a1923aa2ee465838bdba34
dev-mlb <19797865+dev-mlb@users.noreply.github.com>
2024-10-24 08:11:27 -0400

    errorprone :: activate

--- src/test/java/emissary/transform/decode/JsonEscapeTest.java
+++ src/test/java/emissary/transform/decode/JsonEscapeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Deprecated(forRemoval = true)
+@Deprecated
 class JsonEscapeTest extends UnitTest {
     @Test
     void testEscapedAngleBracketChars() {
