3f13c431567c87a9cf237407fc3ed918ff3b5911
dev-mlb <19797865+dev-mlb@users.noreply.github.com>
2024-10-24 11:48:10 -0400

    remove forRemoval

--- src/main/java/emissary/util/DateTimeFormatParserLegacy.java
+++ src/main/java/emissary/util/DateTimeFormatParserLegacy.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Deprecated(forRemoval = true)
+@Deprecated
 @SuppressWarnings("all")
 public class DateTimeFormatParserLegacy {
 
