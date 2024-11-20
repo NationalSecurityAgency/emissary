f6de88ab3c37c5dffb0700674563005138fc63cb
Connor <connor@connortech.me>
2024-11-20 05:23:39 -0500

    fix format

--- src/main/java/emissary/place/CoordinationPlace.java
+++ src/main/java/emissary/place/CoordinationPlace.java
@@ -322,9 +322,9 @@ public class CoordinationPlace extends ServiceProviderPlace {
             if (pushForm) {
                 d.pushCurrentForm(outputForm);
             } else {
-
                 if (logger.isDebugEnabled()) {
-                    logger.debug("Object has {} number of forms in the Stack. CoordinationPlace Setting top form from {} to {}", d.currentFormSize(), d.currentForm(), outputForm);
+                    logger.debug("Object has {} number of forms in the Stack. CoordinationPlace Setting top form from {} to {}", d.currentFormSize(),
+                            d.currentForm(), outputForm);
                 }
                 d.setCurrentForm(outputForm);
             }
