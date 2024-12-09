e5964648469c486d70b4fabdf8f31376a0e6ad7a
djresni <djresni@uwe.nsa.gov>
2024-12-09 21:33:20 +0000

    refactor regex

--- src/main/java/emissary/command/converter/PriorityDirectoryConverter.java
+++ src/main/java/emissary/command/converter/PriorityDirectoryConverter.java
@@ -6,6 +6,9 @@ import emissary.pickup.PriorityDirectory;
 import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine.ITypeConverter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PriorityDirectoryConverter implements ITypeConverter<PriorityDirectory> {
 
     public PriorityDirectoryConverter() {
@@ -13,12 +16,14 @@ public class PriorityDirectoryConverter implements ITypeConverter<PriorityDirect
     }
 
     public static final String PRIORITY_DIR_REGEX = ".*:\\d+$";
+    private static final Pattern priorityDirRegex = Pattern.compile(PRIORITY_DIR_REGEX);
 
     @Override
     public PriorityDirectory convert(String value) {
         final String dirName;
         final int priority;
-        if (value.matches(PRIORITY_DIR_REGEX)) {
+        Matcher matcher = priorityDirRegex.matcher(value);
+        if (matcher.matches()) {
             dirName = StringUtils.substringBeforeLast(value, ":");
             priority = Integer.parseInt(StringUtils.substringAfterLast(value, ":"));
         } else {
