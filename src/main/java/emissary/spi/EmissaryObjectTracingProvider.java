058d64cfa1874bbd83fd2948392ce483da5d006b
Connor <connor@connortech.me>
2025-01-07 14:33:09 -0500

    add dropoff event to ObjectTracing

--- src/main/java/emissary/spi/EmissaryObjectTracingProvider.java
+++ src/main/java/emissary/spi/EmissaryObjectTracingProvider.java
@@ -7,9 +7,19 @@ import java.util.Map;
 public class EmissaryObjectTracingProvider implements ObjectTracing {
 
     @Override
-    public void getObjectTraceFields(IBaseDataObject d, String filename, Stage stage, Map<String, String> fieldMap) {
+    public void getObjectTracePickUpFields(IBaseDataObject d, String filename, Stage stage, Map<String, String> fieldMap) {
         fieldMap.put("inputFileName", filename);
         fieldMap.put("stage", String.valueOf(stage));
+    }
+
+    @Override
+    public void getObjectTraceDropOffFields(IBaseDataObject d, String filename, Stage stage, String filterName, String outputFileName,
+            Map<String, String> fieldMap) {
+        fieldMap.put("inputFileName", filename);
+        fieldMap.put("stage", String.valueOf(stage));
+        fieldMap.put("outputFileName", outputFileName);
+        fieldMap.put("uuid", String.valueOf(d.getInternalId()));
+        fieldMap.put("outputType", filterName);
     }
 
     @Override
