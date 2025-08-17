18443c7cb1a8b1fa7300bdd5e4002b60799340ce
Connor <connor@connortech.me>
2025-08-17 18:28:29 -0400

    add Prometheus integration for health and metrics endpoints

--- src/main/java/emissary/server/api/MetricsAction.java
+++ src/main/java/emissary/server/api/MetricsAction.java
@@ -3,13 +3,19 @@ package emissary.server.api;
 import emissary.core.MetricsManager;
 import emissary.core.NamespaceException;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.exporter.common.TextFormat;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 @Path("")
 // context is /api, set in EmissaryServer
@@ -18,14 +24,33 @@ public class MetricsAction {
 
     @GET
     @Path("/metrics")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response clusterAgents() {
+    public Response metrics(@QueryParam("format") String format) {
         try {
-
-            return Response.ok().entity(MetricsManager.lookup().getMetricRegistry()).build();
-        } catch (NamespaceException ex) {
-            logger.warn("Could not lookup MetricsManager", ex);
-            return Response.serverError().entity("Could not lookup MetricsManager").build();
+            if ("prometheus".equals(format)) {
+                return prometheusMetrics();
+            } else {
+                return jsonMetrics();
+            }
+        } catch (Exception e) {
+            logger.warn("Could not retrieve metrics", e);
+            return Response.serverError().entity("Could not retrieve metrics: " + e.getMessage()).build();
         }
+    }
+
+    private static Response jsonMetrics() throws NamespaceException {
+        return Response.ok()
+                .entity(MetricsManager.lookup().getMetricRegistry())
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    private Response prometheusMetrics() throws NamespaceException, IOException {
+        CollectorRegistry registry = new CollectorRegistry();
+        registry.register(new DropwizardExports(MetricsManager.lookup().getMetricRegistry()));
+        StringWriter writer = new StringWriter();
+        TextFormat.writeFormat(TextFormat.CONTENT_TYPE_004, writer, registry.metricFamilySamples());
+        return Response.ok(writer.toString())
+                .type(TextFormat.CONTENT_TYPE_004)
+                .build();
     }
 }
