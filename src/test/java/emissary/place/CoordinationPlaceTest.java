979744dafde654698425f7cb0a8136e5871a92d0
rupert-griffin <167495366+rupert-griffin@users.noreply.github.com>
2025-10-21 09:30:56 -0400

    New CoordinationPlace configs to deny places or put them at end of ref list (#1171)

--- src/test/java/emissary/place/CoordinationPlaceTest.java
+++ src/test/java/emissary/place/CoordinationPlaceTest.java
@@ -1,12 +1,13 @@
 package emissary.place;
 
+import emissary.config.ConfigUtil;
 import emissary.core.DataObjectFactory;
+import emissary.core.EmissaryException;
 import emissary.core.IBaseDataObject;
 import emissary.core.MobileAgent;
 import emissary.core.Namespace;
 import emissary.core.ResourceWatcher;
 import emissary.test.core.junit5.UnitTest;
-import emissary.util.io.ResourceReader;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.junit.jupiter.api.AfterEach;
@@ -14,9 +15,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -34,8 +36,7 @@ class CoordinationPlaceTest extends UnitTest {
     public void setUp() throws Exception {
         super.setUp();
 
-        InputStream configStream = new ResourceReader().getConfigDataAsStream(this);
-        place = new CoordinationPlace(configStream);
+        place = new CoordinationPlace("emissary.place.CoordinationPlace.cfg");
 
         mockCoordPlace = mock(IServiceProviderPlace.class);
         place.placeRefs = Collections.singletonList(mockCoordPlace);
@@ -80,6 +81,51 @@ class CoordinationPlaceTest extends UnitTest {
         List<IBaseDataObject> sprouts = place.processHeavyDuty(ibdo);
         assertTrue(CollectionUtils.isNotEmpty(sprouts) && sprouts.size() == 1);
         assertTrue(ibdo.getAllCurrentForms().contains("TESTCOORDINATE"));
+    }
+
+    @Test
+    void testPlaceReferenceOrder() throws IOException {
+        place.configG = ConfigUtil.getConfigInfo(CoordinationPlaceTest.class);
+        place.configurePlace();
+
+        List<String> expectedRefs = List.of(
+                "emissary.place.sample.CachePlace",
+                "emissary.place.sample.ToLowerPlace",
+                "emissary.place.sample.ToUpperPlace");
+
+        List<String> actualRefs = place.placeRefs.stream()
+                .map(IServiceProviderPlace::getPlaceName)
+                .collect(Collectors.toList());
+
+        assertEquals(expectedRefs, actualRefs);
+    }
+
+    @Test
+    void testPlaceReferenceOrderWithFlavor() throws EmissaryException, IOException {
+        String originalFlavor = System.getProperty(ConfigUtil.CONFIG_FLAVOR_PROPERTY);
+
+        try {
+            System.setProperty(ConfigUtil.CONFIG_FLAVOR_PROPERTY, String.join(",", originalFlavor, "FLAVORTEST"));
+            ConfigUtil.initialize();
+
+            place.configG = ConfigUtil.getConfigInfo(CoordinationPlaceTest.class);
+            place.configurePlace();
+
+            List<String> expectedRefs = List.of(
+                    "emissary.place.sample.ToLowerPlace",
+                    "emissary.place.sample.ToUpperPlace",
+                    "emissary.place.sample.RefreshablePlace");
+
+            List<String> actualRefs = place.placeRefs.stream()
+                    .map(IServiceProviderPlace::getPlaceName)
+                    .collect(Collectors.toList());
+
+            assertEquals(expectedRefs, actualRefs);
+
+        } finally {
+            System.setProperty(ConfigUtil.CONFIG_FLAVOR_PROPERTY, originalFlavor);
+            ConfigUtil.initialize();
+        }
     }
 
     @Test
