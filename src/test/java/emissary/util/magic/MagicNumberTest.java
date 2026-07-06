99b21c14df15906ba262150a94cbf5c6c5af9917
dev-mlb <19797865+dev-mlb@users.noreply.github.com>
2026-07-06 14:37:38 -0400

    added unit tests to MagicNumberTest

--- src/test/java/emissary/util/magic/MagicNumberTest.java
+++ src/test/java/emissary/util/magic/MagicNumberTest.java
@@ -3,6 +3,8 @@ package emissary.util.magic;
 import emissary.test.core.junit5.UnitTest;
 
 import jakarta.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -84,12 +86,17 @@ class MagicNumberTest extends UnitTest {
     }
 
     @Test
-    void testLessEqualBelong() throws ParseException {
+    void testLessEqualBelong() throws ParseException, DecoderException {
         // ABCD
         MagicNumber m = MagicNumberFactory.buildMagicNumber("0 belong <=0x41424344 FOO");
         assertTrue(m.test("ABCC".getBytes()), "LessEqual magic operator failed");
         assertTrue(m.test("ABCD".getBytes()), "LessEqual magic operator failed");
         assertFalse(m.test("ABCE".getBytes()), "LessEqual magic operator failed");
+
+        m = MagicNumberFactory.buildMagicNumber("0 belong <=0x00010000 FOO");
+        assertTrue(m.test(Hex.decodeHex("00010000")), "Big Endian less than equal failed on exact match");
+        assertTrue(m.test(Hex.decodeHex("0000FFFF")), "Big Endian less than equal failed");
+        assertFalse(m.test(Hex.decodeHex("00020000")), "Big Endian less than equal failed");
     }
 
     @Test
@@ -101,12 +108,23 @@ class MagicNumberTest extends UnitTest {
     }
 
     @Test
-    void testGreaterThanBeshort() throws ParseException {
+    void testGreaterThanBeshort() throws ParseException, DecoderException {
         // AB
         MagicNumber m = MagicNumberFactory.buildMagicNumber("0 beshort >0x4142 FOO");
         assertTrue(m.test("AC".getBytes()), "Greater than magic operator failed");
         assertFalse(m.test("AB".getBytes()), "Greater than magic operator failed");
         assertFalse(m.test("AA".getBytes()), "Greater than magic operator failed");
+
+        m = MagicNumberFactory.buildMagicNumber("0 beshort >0x0000 FOO");
+        assertFalse(m.test(Hex.decodeHex("0000")), "Greater than magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("0001")), "Greater than magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("0101")), "Greater than magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("0100")), "Greater than magic operator failed");
+
+        m = MagicNumberFactory.buildMagicNumber("0 beshort >0x0100 FOO");
+        assertTrue(m.test(Hex.decodeHex("0200")), "Big Endian greater than failed on MSB");
+        assertFalse(m.test(Hex.decodeHex("00FF")), "Big Endian greater than failed on MSB tie-breaker");
+        assertTrue(m.test(Hex.decodeHex("0101")), "Big Endian greater than failed on LSB tie-breaker");
     }
 
     @Test
@@ -134,6 +152,48 @@ class MagicNumberTest extends UnitTest {
         assertTrue(m.test("AA".getBytes()), "LessEqual magic operator failed");
         assertTrue(m.test("AB".getBytes()), "LessEqual magic operator failed");
         assertFalse(m.test("AC".getBytes()), "LessEqual magic operator failed");
+    }
+
+    @Test
+    void testGreaterThanLeshort() throws ParseException, DecoderException {
+        MagicNumber m = MagicNumberFactory.buildMagicNumber("0 leshort >0x0001 FOO");
+        assertTrue(m.test(Hex.decodeHex("0200")), "Little Endian greater than failed on LSB increment");
+        assertTrue(m.test(Hex.decodeHex("0001")), "Little Endian greater than failed on MSB increment");
+        assertFalse(m.test(Hex.decodeHex("0000")), "Little Endian greater than failed on lower value");
+    }
+
+    @Test
+    void testLessThanEqualLelong() throws ParseException, DecoderException {
+        MagicNumber m = MagicNumberFactory.buildMagicNumber("0 lelong <=0x00000100 FOO");
+        assertTrue(m.test(Hex.decodeHex("00010000")), "Little Endian less than equal failed on exact match");
+        assertTrue(m.test(Hex.decodeHex("FF000000")), "Little Endian less than equal failed on lower value");
+        assertFalse(m.test(Hex.decodeHex("01010000")), "Little Endian less than equal failed on higher value");
+    }
+
+    @Test
+    void testSameInputBigEndianVsLittleEndian() throws ParseException, DecoderException {
+        MagicNumber bem = MagicNumberFactory.buildMagicNumber("0 beshort >0x0005 FOO");
+        assertTrue(bem.test(Hex.decodeHex("0200")), "Big Endian failed: 0x0200 (512) should be greater than 5");
+
+        MagicNumber lem = MagicNumberFactory.buildMagicNumber("0 leshort >0x0005 FOO");
+        assertFalse(lem.test(Hex.decodeHex("0200")), "Little Endian failed: 0x0200 (2) should NOT be greater than 5");
+    }
+
+    @Test
+    void testOrBeshort() throws ParseException, DecoderException {
+        MagicNumber m = MagicNumberFactory.buildMagicNumber("0 beshort x0x1010 FOO");
+
+        assertFalse(m.test(Hex.decodeHex("0000")), "OR magic operator failed");
+        assertFalse(m.test(Hex.decodeHex("0001")), "OR magic operator failed");
+        assertFalse(m.test(Hex.decodeHex("0101")), "OR magic operator failed");
+
+        assertTrue(m.test(Hex.decodeHex("0010")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("0011")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("1000")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("1010")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("1100")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("1111")), "OR magic operator failed");
+        assertTrue(m.test(Hex.decodeHex("FFFF")), "OR magic operator failed");
     }
 
     @Test
